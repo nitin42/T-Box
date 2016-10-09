f56098c085f757779f5a0adfc1fc62d1b7273473
Nitin Tulswani <tulswani19@gmail.com>
2016-10-10 01:22:18 +0530

    removed access token

---
+++ utils/token.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var Dropbox = require('dropbox');
+var dbx = new Dropbox({ accessToken: **************************************************************** });
+
+module.exports = {
+	dbx: dbx
+};
