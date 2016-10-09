edc579943b900dcd96b31fed1de67b37a6c205c0
Nitin Tulswani <tulswani19@gmail.com>
2016-10-10 01:54:29 +0530

    scary

--- utils/token.js
+++ utils/token.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Dropbox = require('dropbox');
-var dbx = new Dropbox({ accessToken: **************************************************************** });
+var dbx = new Dropbox({ accessToken: 'dnauvo0Y1yAAAAAAAAACvg4Y6B3rH7eaSOU5NB21VOEuKbaLL6eGMYYkSCiDs1k-' });
 
 module.exports = {
 	dbx: dbx
